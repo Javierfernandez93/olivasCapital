412a5a451ae343332a43be7f5a3f14ae6b00c8f4
Javier Fernández <leqjl93@hotmail.com>
2023-04-19 21:16:28 -0600

    chores

--- layout/admin.layout.php
+++ layout/admin.layout.php
@@ -398,8 +398,8 @@
     <script src="../../src/js/plugins/chartjs.min.js" type="text/javascript"></script>
     <script src="../../src/js/42d5adcbca.js" type="text/javascript"></script>
     <script src="../../src/js/jquery-3.1.1.js" type="text/javascript"></script>
-    <script src="../../src/js/general.js?t=3" type="text/javascript"></script>
-    <script src="../../src/js/alertCtrl.js?t=1" type="text/javascript"></script>
+    <script src="../../src/js/general.js?t=1.0.3" type="text/javascript"></script>
+    <script src="../../src/js/alertCtrl.js?t=1.0.3" type="text/javascript"></script>
 
     <script>
     var win = navigator.platform.indexOf('Win') > -1;
--- src/js/adminTransactions.vue.js
+++ src/js/adminTransactions.vue.js
@@ -1,4 +1,4 @@
-import { AdmintransactionsViewer } from '../../src/js/admintransactionsViewer.vue.js?t=1.0.2'
+import { AdmintransactionsViewer } from '../../src/js/admintransactionsViewer.vue.js?t=1.0.3'
 
 Vue.createApp({
     components : { 
